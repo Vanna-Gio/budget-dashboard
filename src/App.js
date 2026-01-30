59dd5a9326012984949ded62be110822483dbae7
Sovanna Ra <sovann.ra277@gmail.com>
2026-01-31 06:49:38 +0700

    feat: add filter functionality with useState hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
+import { useState } from 'react';
 import './App.css';
 import BudgetCard from './components/BudgetCard';
 import './components/BudgetCard.css';
 import { budgetStats } from './data/mockData';
 
 function App() {
+  const [filter, setFilter] = useState('all'); // State: 'all' , 'Positive',
+
+  //Filter logic 
+  const filteredStats = budgetStats.filter((stat) => {
+    if (filter === 'all') return true;
+    if (filter === 'positive') return stat.growthPositive === true;
+    if (filter === 'negative') return stat.growthPositive === false;
+    return true;
+  });
   return (
     <div className="App">
-      <h1>Budget Dashboard</h1>
-      <div className="card-grid">
-        {budgetStats.map((stat) =>(
-            <BudgetCard
-              key={stat.id}
-              {...stat}
-          />
-        ))}
-        
-        
+      <div className='header' >
+        <h1>Budget Dashboard</h1>
+        <div className="filter-buttons">
+          <button 
+            className={filter === 'all' ? 'active' : ''}
+            onClick={() => setFilter('all')}
+            >
+              All ({budgetStats.length})
+            </button>
+          <button 
+            className={filter === 'positive' ? 'active' : ''}
+            onClick={() => setFilter('positive')}
+            >
+              Growth 
+            </button>
+          <button 
+            className={filter === 'negative' ? 'active' : ''}
+            onClick={() => setFilter('negative')}
+            >
+              Declining
+            </button>
+        </div>
+      </div>
+      <div className='card-grid'>
+        {filteredStats.map((stat) => (
+          <BudgetCard
+            key={stat.id}
+            {...stat}
+            />
+        )
+        )}
       </div>
     </div>
   );
